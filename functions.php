3ea986ac9bfec65c9f299aacdb2c4f73dca5333b
Nemolite <g25092011@mail.ru>
2021-04-14 09:16:20 +0300

    Добавление основных файлов темы

--- functions.php
+++ functions.php
@@ -2,4 +2,11 @@
 /**
  * Функции, хуки темы
  */
+
+function mytheme_scripts_style() {
+
+	wp_enqueue_style( 'mytheme-style', get_template_directory_uri() .'/css/main.css' );
+	wp_enqueue_script( 'mytheme-script', get_template_directory_uri() . '/js/main.js', array(), '1.0.0', true );
+}
+add_action( 'wp_enqueue_scripts', 'mytheme_scripts_style' );
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-
+/**
+ * Основной стартовый файл темы
+ */
 ?>
+<?php get_header(); ?>
+
+<body>
+    <?php echo get_template_directory_uri(); ?>  
+</body>
+
+<?php get_footer(); ?>
